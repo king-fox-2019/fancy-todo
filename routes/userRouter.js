deac37d8f438ec60d747a3edb384e616d03c2b35
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-11-30 04:24:52 +0700

    adding user router, adding jwt ini helpers, and adding user controller

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,5 +2,9 @@
 
 const express = require('express')
 const router = express.Router()
+const { userController } = require('../controllers')
+
+router.post('/register', userController.register)
+router.post('/login', userController.login)
 
 module.exports = router
