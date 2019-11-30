2a8118a2eb581763c40cfc8fcac3c38926e58608
Immanuel Pandiangan <nuelpan@gmail.com>
2019-11-30 18:15:12 +0700

    adding helpers bcrypt and jwt adding middleware auth and errorHandler adding mongoose config for mongodb connection adding application skeleton for development

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 if (process.env.NODE_ENV = 'development') {
     require('dotenv').config()
 }
-
+require('./config/mongoose')
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT
+const cors = require('cors')
+const morgan = require('morgan')
 
+const routes = require('./routes')
+const errorHandler = require('./middleware/errorHandler')
 
-app.listen(PORT,() => {console.log('listening at port:', PORT)})
+app.use(cors())
+app.use(morgan('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', routes)
+app.use(errorHandler)
+
+module.exports = app
