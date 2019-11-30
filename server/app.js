f3e709726fe2faacb2a59bcd6cef6f4b7bf149c1
didadadida93 <didadadida93@gmail.com>
2019-11-30 21:57:39 +0700

    add mongoose to application and creating basic model

--- server/app.js
+++ server/app.js
@@ -2,10 +2,16 @@ if (process.env.NODE_ENV === 'development') require('dotenv').config()
 
 
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const routes = require('./routes')
 
+mongoose.connect('mongodb://localhost:27017/fancytodo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 app.use('/', routes)
 
