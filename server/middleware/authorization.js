ff2d5f8223f179c971a4c0fbae493486afb04e34
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-12-02 03:15:37 +0700

    adding authorization in middleware and user router

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -1,5 +1,20 @@
 'use strict'
 
-module.exports = (req, res, next) => {
+const { Todo } = require('../models')
 
+module.exports = (req, res, next) => {
+  Todo
+    .findById({ _id: req.params.id })
+    .then(todo => {
+      if (String(todo.userId) == String(req.decoded._id)) {
+        next()
+      } else {
+        next({ status: 403, message: `You dont have authorize to do that` })
+      }
+    })
+    .catch(err => { next(err) })
 }
+
+
+
+
--- server/routes/todoRouter.js
+++ server/routes/todoRouter.js
@@ -3,11 +3,11 @@
 const express = require('express')
 const router = express.Router()
 const { todoController } = require('../controllers')
-// const authorization = require('../middleware/authorization')
+const authorization = require('../middleware/authorization')
 
 router.post('/', todoController.create)
 router.get('/', todoController.read)
-router.put('/:id', todoController.update)
-router.delete('/:id', todoController.delete)
+router.put('/:id', authorization, todoController.update)
+router.delete('/:id', authorization, todoController.delete)
 
 module.exports = router
